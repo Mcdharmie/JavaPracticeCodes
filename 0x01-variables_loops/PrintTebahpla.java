283211395ad138edeb84e89aca0a1974e6e5703f
Mcdharmie <damilola.afolayan07@gmail.com>
2023-11-24 19:13:46 +0000

    A program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_loops/PrintTebahpla.java
@@ -0,0 +1,12 @@
+
+
+import static java.lang.System.out;
+
+public class PrintTebahpla{
+	public static void main(String... args){
+		for (char alphabet ='z'; alphabet >= 'a'; alphabet--){
+			out.print(alphabet);
+		}
+	}
+}
+
