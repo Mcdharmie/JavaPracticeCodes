850b1fbe007fc81ca2a8c093aea03ec16e00e022
Mcdharmie <damilola.afolayan07@gmail.com>
2023-11-24 18:49:14 +0000

    A program that prints the alphabet in lowercase, and then in uppercase

---
+++ 0x01-variables_loops/PrintAlphabet3.java
@@ -0,0 +1,20 @@
+
+
+import static java.lang.System.out;
+
+public class PrintAlphabet3{
+	public static void main(String... args){
+		for (char alphabet2 ='a'; alphabet2 <= 'z'; alphabet2++){
+			out.print(alphabet2);
+		}out.println(" ");
+
+		for (char alphabet ='A'; alphabet <= 'Z'; alphabet++){
+			out.print(alphabet);
+		}
+
+
+
+	}
+}
+
+
