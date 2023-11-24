3cea2beae5743e32354503ce388dec95fde19372
Mcdharmie <damilola.afolayan07@gmail.com>
2023-11-24 19:21:21 +0000

    A program that prints all the numbers of base 16 in uppercase

---
+++ 0x01-variables_loops/PrintBase16.java
@@ -0,0 +1,14 @@
+
+
+import static java.lang.System.out;
+
+public class PrintBase16 {
+	public static void main(String... args) {
+		for (int  num = 0; num <= 9; num++) {
+			out.print(num);
+		}
+		for (char alphabet = 65; alphabet <= 69; alphabet++) {
+			out.print(alphabet);
+		}
+	}
+}
