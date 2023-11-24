d3d5dbce0e91cdc9af3b0a136f6b2b0771e57621
Mcdharmie <damilola.afolayan07@gmail.com>
2023-11-24 18:56:50 +0000

    A  program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_loops/PrintNumber.java
@@ -0,0 +1,11 @@
+
+
+import static java.lang.System.out;
+
+public class PrintNumber{
+	public static void main(String... args){
+		for (int num =0; num <= 10; num++){
+			out.print(num);
+		}
+	}
+}
