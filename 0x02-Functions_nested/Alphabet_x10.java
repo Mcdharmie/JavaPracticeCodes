96a4d152d3e70a5573cb9e87acf7b6b1a62e616a
Mcdharmie <damilola.afolayan07@gmail.com>
2024-02-25 16:16:32 +0000

    A method that prints 10 times the alphabet, in lowercase

---
+++ 0x02-Functions_nested/Alphabet_x10.java
@@ -0,0 +1,15 @@
+
+public class Alphabet_x10{
+	public static void main(String[] args){
+		alphabet();
+	}
+
+	public static void alphabet(){
+		for (int i=0; i < 10; i++){
+			for (char j ='a'; j <= 'z'; j++){
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		}
+	}
+}
