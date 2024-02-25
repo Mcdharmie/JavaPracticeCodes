2d61f99845eb5e16a7853c8a11c731d4cfbed841
Mcdharmie <damilola.afolayan07@gmail.com>
2024-02-25 12:54:47 +0000

    A function that prints the alphabet, in lowercase

---
+++ 0x02-Functions_nested/Alphabet.java
@@ -0,0 +1,12 @@
+
+public class Alphabet{
+	public static void main(String[] args){
+		alphabet();
+	}
+
+	public static void alphabet(){
+		for (char i='a'; i <='z'; i++){
+			System.out.print(i);
+		}
+	}
+}
