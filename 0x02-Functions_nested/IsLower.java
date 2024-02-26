8f121dec22117b4b76db47d956c76196a233e6a7
Mcdharmie <damilola.afolayan07@gmail.com>
2024-02-26 07:45:41 +0000

    A method that checks for lowercase character

---
+++ 0x02-Functions_nested/IsLower.java
@@ -0,0 +1,13 @@
+public class IsLower{
+
+	public static void main(String[] args){
+		char letter = 'a';
+
+		if(letter >= 'a' && letter <= 'z'){
+			System.out.print(1);
+		}
+		else{
+			System.out.print(0);
+		}
+	}
+}
